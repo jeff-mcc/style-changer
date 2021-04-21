97b60eada7aade9abe031143b5cb7669ce07a148
jeff-mcc <reffery@gmail.com>
2021-04-21 13:20:50 -0600

    completed code-along

--- src/components/ColorChanger.js
+++ src/components/ColorChanger.js
@@ -3,10 +3,11 @@ import React, { Component } from 'react';
 export default class ColorChanger extends Component {
   render() {
     return (
-      <select className="dropDownContainer">
+      <select onChange={(e)=>this.props.update(e.target.value)} disabled={this.props.allowEdit==='false'?true:false} className="dropDownContainer">
         <option value="black"> Black </option>
         <option value="blue"> Blue </option>
         <option value="green"> Green </option>
+        <option value="purple"> Purple </option>
       </select>
     );
   }
--- src/components/SizeChanger.js
+++ src/components/SizeChanger.js
@@ -3,10 +3,11 @@ import React, { Component } from 'react';
 export default class ColorChanger extends Component {
   render() {
     return (
-      <select className="dropDownContainer">
+      <select onChange={(e)=>this.props.update(+e.target.value)} disabled={this.props.allowEdit==='false'?true:false} className="dropDownContainer">
         <option value="12"> 12 </option>
-        <option value="13"> 13 </option>
         <option value="14"> 14 </option>
+        <option value="18"> 18 </option>
+        <option value="24"> 24 </option>
       </select>
     );
   }
